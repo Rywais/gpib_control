88cec345fd53e82c12b72e28216dd2292e57ba26
Ryan Watt <rywais@gmail.com>
2018-05-24 16:02:59 -0700

    Initial commit

---
+++ gpib.py
@@ -0,0 +1,24 @@
+import visa
+
+class Gpib_communicator:
+    def __init__(self):
+        self.address = ""
+        self.rm = visa.ResourceManager()
+        self.inst = None
+
+    def set_rm(self, rm):
+        self.rm = visa.ResourceManager(rm)
+
+    def set_address(self, address):
+        self.address = address
+        self.inst = self.rm.open_resource(address)
+
+    def get_address(self):
+        return self.address
+
+    def write(self, message):
+        self.inst.write(message)
+
+    def query(self, message):
+        return self.inst.query(message)
+
