a7be5cb85f43e2c3789b9d0a8d230021ebe14376
Ryan W <rywais@gmail.com>
2018-06-01 17:48:18 -0700

    Added prologix USB controller support

---
+++ prologix_gpib.py
@@ -0,0 +1,47 @@
+import serial, time
+
+class Gpib_communicator:
+    def __init__(self):
+        self.address = ""
+        ser = None
+
+    def set_serial(self, serial_port):
+        self.ser = serial.Serial(serial_port)
+        self.ser.timeout = 0.001
+    
+    def get_serial(self):
+        return self.ser.port
+
+    def write(self, message):
+        self.ser.write(message.encode('utf-8'))
+
+    def read(self, timeout=0.0, term_char='\n'):
+        msg = ''
+        self.write('++read\n')
+        start = time.perf_counter()
+        while True: #Wait for first data to be available
+            b = self.ser.read()
+            if b != b'':
+                msg = msg + b.decode('utf-8')
+                break
+            if time.perf_counter() - start > timeout:
+                return ''
+
+        while True: #Read the rest of the data
+            b = self.ser.read()
+            if b == term_char.encode('utf-8'):
+                msg = msg + b.decode('utf-8')
+                break
+            if b == b'':
+                break
+            msg = msg + b.decode('utf-8')
+        return msg
+
+    def query(self, message, delay=0.0, timeout=0.0):
+        self.write(message)
+        time.sleep(delay)
+        return self.read(timeout=timeout)
+
+    def read_stb(self):
+        self.write('++spoll\n')
+        return int(self.read())
